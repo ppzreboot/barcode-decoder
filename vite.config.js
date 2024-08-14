d3b77cfedc4467108cf0bce1377174b1de97054b
ppzreboot <ppzreboot@qq.com>
2024-08-14 13:37:52 +0000

    fix: base path

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/barcode-utils',
+  base: '/barcode-qrcode-utils',
   plugins: [react()],
 })
